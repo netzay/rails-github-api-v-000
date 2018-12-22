9aa563970d6eebbe1f44cc1d8f02048658a6da4c
Ally Netzel <netzelally@gmail.com>
2018-12-22 21:28:26 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,5 @@
 class SessionsController < ApplicationController
-  skip_before_action :authenticate_user
-
+  skip_before_action :authenticate_user, :only => :create
   def create
     resp = Faraday.post("https://github.com/login/oauth/access_token") do |req|
       req.params["client_id"] = ENV["GITHUB_CLIENT_ID"]
