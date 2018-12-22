220faa3bedc4c897ff265cc0beeb5bd96bdf9c22
Ally Netzel <netzelally@gmail.com>
2018-12-22 21:13:21 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
     client_secret = ENV['Github_Client_Secret']
     redirect_uri = CGI.escape("http://localhost:3000/auth")
     github_url = "https://github.com/login/oauth/authorize"
-    redirect_to foursquare_url unless logged_in?
+    redirect_to github_url unless logged_in?
   end
 
   def logged_in?
