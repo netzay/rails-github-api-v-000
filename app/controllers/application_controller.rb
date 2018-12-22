5056f79b6bf3ca3a922a733dcbe50b8a7ffc23d9
Ally Netzel <netzelally@gmail.com>
2018-12-22 21:24:32 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
 
   
   def authenticate_user
-    client_id = ENV['Github_Client_ID']
+    client_id = ENV['GITHUB_CLIENT_ID']
     client_secret = ENV['Github_Client_Secret']
     redirect_uri = CGI.escape("http://localhost:3000/auth")
     github_url = "https://github.com/login/oauth/authorize?scope=repo&client_id=#{client_id}&redirect_uri=#{redirect_uri}"
